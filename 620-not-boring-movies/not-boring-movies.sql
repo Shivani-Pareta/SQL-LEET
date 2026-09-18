2f2930329f06ecfaf501fed25f5e9d5219ae3401
shivani.pareta1305 <shivani.pareta1305@gmail.com>
2026-09-18 17:39:57 +0530

    Time: 253 ms (93.37%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,9 +1,13 @@
 # Write your MySQL query statement below
 SELECT * FROM Cinema
--- CASE
 WHERE id % 2 !=0 AND description != 'boring'
 ORDER BY rating DESC
---     WHEN description != 'boring'
---     ELSE 'NULL'
--- END
 
+
+-- SELECT id, movie, description, rating
+-- FROM Cinema
+-- WHERE CASE 
+--         WHEN id % 2 != 0 AND description != 'boring' THEN 1 
+--         ELSE 0 
+--       END = 1
+-- ORDER BY rating DESC;
