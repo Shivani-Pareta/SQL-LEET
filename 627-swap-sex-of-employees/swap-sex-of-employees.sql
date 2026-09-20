efe890f3559bea11f902b62b041f799a649ab130
shivani.pareta1305 <shivani.pareta1305@gmail.com>
2026-09-20 14:15:28 +0530

    Time: 286 ms (40.57%) | Memory: 0B (100.00%) - LeetSync

---
+++ 627-swap-sex-of-employees/swap-sex-of-employees.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+UPDATE Salary
+-- SELECT id,name,sex,salary 
+SET sex = CASE 
+    WHEN sex = 'f' THEN 'm'
+    WHEN sex = 'm' THEN 'f'
+    ELSE sex
+END 
+-- FROM Salary
