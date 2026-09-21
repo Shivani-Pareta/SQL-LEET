fd42574c74f3ef7c7b507ccdfca1d8591ebf0f55
shivani.pareta1305 <shivani.pareta1305@gmail.com>
2026-09-21 15:45:21 +0530

    Time: 1197 ms (74.87%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -5,7 +5,6 @@
 -- JOIN Sales S
 -- ON S.product_id = P.product_id
 -- WHERE S.sale_date BETWEEN '2019-01-01' AND '2019-03-31'
--- GROUP BY P.product_id,P.product_name
 
 #for the only sold in first quarter having clause is important as it is deciding the date range it is supposed to be in where clause will work when we can consider if the product is sold even once in the first quarter but since product 2 was sold even after that our solution requires the having clause.
 
